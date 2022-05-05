54bbcabc60f8a0a7e6be54fbde58fddc5581e45d
miguel0920 <miguelbarahona00@gmail.com>
2022-05-05 14:51:05 -0500

    validation command with FluentValidation

---
+++ OnionRESTFull/Application/Features/Customer/Commands/CreateCustomerCommand/CreateCustomerCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Customer.Commands.CreateCustomerCommand
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.")
+                .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.")
+                .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.BirthdayDate)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.");
+
+            RuleFor(p => p.Phone)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.")
+                .Matches(@"^\d{10,10}$").WithMessage("{PropertyName} debe cumplir el formato 0000000000")
+                .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.Email)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.")
+                .EmailAddress().WithMessage("{PropertyName} debe ser una dirección de email valida.")
+                .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.Address)
+                .NotEmpty().WithMessage("{PropertyName} no puede ser vacío.")
+                .MaximumLength(80).WithMessage("{PropertyName} no debe exceder de {MaxLength} caracteres.");
+        }
+    }
+}
